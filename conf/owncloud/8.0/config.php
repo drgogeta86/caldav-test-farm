e9ebc2686bd3d5d6926945d380f3434eeee9bace
dr_gogeta86 <dr_gogeta86@messinalug.org>
2016-10-24 22:27:25 +0200

    Added Nginx as reverse proxy, Configured OwnCloud with sqlite

---
+++ conf/owncloud/8.0/config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocmzs49nt52i',
+  'passwordsalt' => 'BbmjLW/qSEWbnDTkjiAja1uXWSOY0z',
+  'secret' => 'IWNlmPQAymnvc8dm7cpqlJ8H7F9alYpNTxhdRRdufjsLPRDA',
+  'trusted_domains' => 
+  array (
+    0 => '8.0.tstsrv.in.merproject.org',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://8.0.tstsrv.in.merproject.org',
+  'dbtype' => 'sqlite3',
+  'version' => '8.0.15.1',
+  'installed' => true,
+);
+
---
+++ conf/owncloud/8.2/config.php
@@ -0,0 +1,17 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3aaxzp9272',
+  'passwordsalt' => 'feqQRBjLUeTJnVijKPpUb/3YsdG7vg',
+  'secret' => 'HVM31gFxGfbQHRRCsRNkQQ8aFPgAnRtnHOUdfC6OAW/QyElI',
+  'trusted_domains' => 
+  array (
+    0 => '8.2.tstsrv.in.merproject.org',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'overwrite.cli.url' => 'http://8.2.tstsrv.in.merproject.org',
+  'dbtype' => 'sqlite3',
+  'version' => '8.2.8.2',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
+
